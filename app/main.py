83428097eea4c0dded8aee243dd5451f6ae2577e
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-06-10 12:38:01 +0300

    Все виправлено

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def count_occurrences(phrase: str, letter: str) -> int:
         phrase_lover = phrase.lower()
-        counter = phrase_lover.count(letter)
+        counter = phrase_lover.count(letter.lower())
         return counter
 print(count_occurrences("Samsung is gnusmas", "s"))

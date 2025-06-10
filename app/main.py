1be4a329bb2b76e1f3c37b0c049b1785d8612405
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-06-10 12:49:03 +0300

    Закоментовані тести

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ def count_occurrences(phrase: str, letter: str) -> int:
         phrase_lover = phrase.lower()
         counter = phrase_lover.count(letter.lower())
         return counter
-print(count_occurrences("Samsung is gnusmas", "s"))
+#print(count_occurrences("Samsung is gnusmas", "s"))

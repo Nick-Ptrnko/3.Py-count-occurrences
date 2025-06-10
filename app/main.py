edf850fe1a97fd420b7793e2d77e59af65f5e377
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-06-10 11:24:22 +0300

    Лише врахування регістру у фразі

--- app/main.py
+++ app/main.py
@@ -1,3 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
-        return phrase.count(letter)
+        return phrase.lower().count(letter)
 print(count_occurrences("Samsung is gnusmas", "s"))
